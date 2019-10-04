ecd1f585a5dcdfcd27a4e9828f321e8f5c2a0bea
bibin <bibinkurian2@gmail.com>
2019-10-04 01:25:04 -0400

    flexiblel1l2cachesimulator

--- sim_cache.cpp
+++ sim_cache.cpp
@@ -14,13 +14,15 @@ unsigned int line=0;
 class Block {
     public:    
     bool is_dirty=false;
-    bool is_valid=false;   
-    int tag_selector=0;     
+    bool is_valid=false;
+    unsigned int tag_selector=0;     
 };
 
-bool CompareTimestamps(Sector a, Sector b){
-    return a.timestamp>b.timestamp;
-}
+class Tag {
+    public:
+    unsigned int tag_id;
+    bool is_valid = false;
+};
 
 class Sector{
     public:
@@ -29,13 +31,18 @@ class Sector{
             blocks.push_back(Block());
         }
         for(int i=0;i<addr_tags;i++){
-            tags.push_back(0);
+            tags.push_back(Tag());
         }
     }
     vector<Block> blocks;
-    vector<int> tags;
-    unsigned int timestamp;
+    vector<Tag> tags;
+    bool is_valid=false;
+    unsigned int timestamp=0;
 };
+
+bool CompareTimestamps(Sector a, Sector b){
+    return a.timestamp>b.timestamp;
+}
 
 class Set{
     public:
@@ -50,14 +57,52 @@ class Set{
 
 class Cache {
     private:
-    int block_size;
-    int cache_size;        
-    int assoc;
-    int data_blocks;
-    int addr_tags;
+    unsigned int block_size;
+    unsigned int cache_size;        
+    unsigned int assoc;
+    
     Cache* next_level       =   nullptr;
     vector<Set> sets;
 
+    void evictSector(vector<Sector>::reverse_iterator sector_to_replace,unsigned int c0,unsigned int c1,unsigned int c2, unsigned int c3, char mode){
+        // blocks associated with the old address tag need to be invalidated
+        unsigned int ind_addr_tag_to_be_evicted = c2;            
+        unsigned int i=0;
+        for(vector<Block>::iterator bl_it=sector_to_replace->blocks.begin();bl_it!=sector_to_replace->blocks.end();bl_it++){
+            if((bl_it->tag_selector==ind_addr_tag_to_be_evicted && c3!=sector_to_replace->tags[bl_it->tag_selector].tag_id) || (i == c0)){    
+                //                    
+                if(bl_it->is_valid)
+                {
+                    if(bl_it->is_dirty)
+                    {
+                        num_write_backs++;
+                        num_memory_access++;
+                        unsigned int old_addr = (((sector_to_replace->tags[ind_addr_tag_to_be_evicted].tag_id*addr_tags+ind_addr_tag_to_be_evicted)*number_of_sets+c1)*data_blocks+i)*block_size;
+                        if(next_level != nullptr){                        
+                            next_level->WriteToAddress(old_addr);
+                        }
+                    }
+                    bl_it->tag_selector=0;;
+                    bl_it->is_valid=false;
+                    bl_it->is_dirty=false;
+                }                
+            }
+            i++;
+        }        
+        sector_to_replace->blocks[c0].is_valid=true;
+        sector_to_replace->tags[c2].is_valid=true;
+        sector_to_replace->blocks[c0].is_dirty=false;
+        if(mode=='w'){
+            sector_to_replace->blocks[c0].is_dirty= true;
+        }
+        sector_to_replace->blocks[c0].tag_selector=c2;
+        sector_to_replace->tags[c2].tag_id=c3;
+        sector_to_replace->timestamp=call_count++;        
+    }
+
+    public:
+    unsigned int data_blocks;
+    unsigned int addr_tags;
     int num_reads                   =   0;
     int num_writes                  =   0;
     int num_read_miss               =   0;
@@ -65,34 +110,30 @@ class Cache {
     int num_write_backs             =   0;
     int num_memory_access           =   0;
 
-    int number_of_sets              =   0;    
-    int total_sectors_in_cache      =   0;   
+    int sector_miss                 =   0;
+    int cache_block_miss            =   0;
 
-    public:
-    Cache(int block_size, int cache_size, int associativity, int data_blocks, int addr_tags): 
-        block_size(block_size), cache_size(cache_size), assoc(associativity), data_blocks(data_blocks), addr_tags(addr_tags){
+    unsigned int number_of_sets              =   0;    
+    unsigned int total_sectors_in_cache      =   0;   
+
+    int cache_level                 =   0;
+
+    
+    
+    Cache(int block_size, int cache_size, int associativity, int data_blocks, int addr_tags, int level): 
+        block_size(block_size), cache_size(cache_size), assoc(associativity), data_blocks(data_blocks), addr_tags(addr_tags), cache_level(level){
             total_sectors_in_cache = cache_size / (block_size*data_blocks);            
             number_of_sets = total_sectors_in_cache/associativity;
-            for(int i=0;i<number_of_sets;i++){
+            for(unsigned int i=0;i<number_of_sets;i++){
                 sets.push_back(Set(associativity,data_blocks,addr_tags));
             }
         }
 
     void AttachNextLevelCache(Cache *c){
         next_level=c;
-    }
-    void DisplayStats(){        
-        cout<<"===== Simulation Results =====\n";
-        cout<<"a. number of L1 reads:			"<<num_reads<<"\n";
-        cout<<"b. number of L1 read misses:		"<<num_read_miss<<"\n";
-        cout<<"c. number of L1 writes:			"<<num_writes<<"\n";
-        cout<<"d. number of L1 write misses:		"<<num_write_miss<<"\n";
-        printf("e. L1 miss rate:			%1.4f\n",((float)num_read_miss+(float)num_write_miss)/((float)num_reads+(float)num_writes));        
-        cout<<"f. number of writebacks from L1 memory:	"<<num_write_backs<<"\n";
-        cout<<"g. total memory traffic:		"<<num_memory_access<<"\n";
-    }
+    }    
 
-    Block FetchBlock(unsigned int address,char mode){
+    void FetchBlock(unsigned int address,char mode){
         if(mode=='r'){
             num_reads++;
         }else if(mode == 'w'){
@@ -105,22 +146,84 @@ class Cache {
         unsigned int c2 =   (c/(data_blocks*number_of_sets))%addr_tags;
         unsigned int c3 =   c/(data_blocks*number_of_sets*addr_tags);
         bool is_found=false;
+        bool is_tag_found = false;
+        bool is_block_found = false;
+
         bool invalid_block_found=false;
+        bool invalid_tag_block_found=false;
         vector<Sector>::reverse_iterator lru_block=sets[c1].sectors.rbegin();
         vector<Sector>::reverse_iterator invalid_block;
-        for(vector<Sector>::reverse_iterator it=sets[c1].sectors.rbegin();it != sets[c1].sectors.rend();it++){            
-            if(it->blocks[c0].is_valid && it->blocks[c0].tag_selector == c2 && it->tags[c2] == c3){
-                is_found=true;
-                it->timestamp=call_count++;
-                return it->blocks[0];
+        vector<Sector>::reverse_iterator invalid_tag_block;
+        
+        for(vector<Sector>::reverse_iterator it=sets[c1].sectors.rbegin();it != sets[c1].sectors.rend();it++)
+        {
+            for(vector<Block>::iterator bl_it=it->blocks.begin(); bl_it != it->blocks.end(); bl_it++){
+                if(bl_it->is_valid){
+                    is_tag_found=true;
+                }
             }
+            if((it->tags[c2].tag_id == c3 && it->tags[c2].is_valid)){
+                
+                if(it->blocks[c0].is_valid && it->blocks[c0].tag_selector == c2 ){
+                    is_found=true;
+                    it->timestamp=call_count++;
+                    if(mode=='w'){
+                        it->blocks[c0].is_dirty= true;
+                    }
+                                   
+                }                                
+            }
+            if(it->blocks[c0].is_valid && it->blocks[c0].tag_selector == c2){
+                is_block_found=true;
+            }            
+
+            //Third would be lru
             if(it->timestamp < lru_block->timestamp){
                 lru_block=it;
             }
-            if(!it->blocks[c0].is_valid && !invalid_block_found){
+            //If block is not found first priority to replace would be for matching tag but invalid block
+            if(it->tags[c2].tag_id==c3 && it->tags[c2].is_valid && !it->blocks[c0].is_valid){
                 invalid_block=it;
                 invalid_block_found=true;
             }
+            //Second best option would be invalid tag
+            if(!it->tags[c2].is_valid  && !invalid_tag_block_found){
+                invalid_tag_block=it;
+                invalid_tag_block_found=true;
+            }
+        }
+        if(cache_level==1){
+            printf("------------------------------------------------\n");
+            if(mode=='r'){
+                printf("# %d: read %0x\n",line,address);
+                printf("L1 read: %0x (tag %0x, index %d)\n", address, c3, c1);
+            }else if (mode=='w'){
+                printf("# %d: write %0x\n",line,address);
+                printf("L1 write: %0x (tag %0x, index %d)\n", address, c3, c1);
+            }            
+            if(is_found){
+                printf("L1 hit \n");
+            }else{
+                printf("L1 miss \n");
+            } 
+        }else if(cache_level==2){
+            if(mode=='r'){
+                printf("L2 read: %0x (C0 %0x, C1 %0x, C2 %0x, C3 %0x) \n",address,c0,c1,c2,c3);
+            }else if(mode == 'w'){
+                printf("L2 write:  (C0 %0x, C1 %0x, C2 %0x, C3 %0x) \n",c0,c1,c2,c3);
+            }
+            
+            if(is_found){
+                printf("L2 hit \n");
+            }else{
+                printf("L2 miss \n");
+            } 
+        }
+        if(!is_block_found){
+            cache_block_miss++;
+        }
+        if(!is_tag_found){
+                sector_miss++;                       
         }
         if(!is_found){  
             num_memory_access++;
@@ -129,74 +232,36 @@ class Cache {
             }else if(mode == 'w'){
                 num_write_miss++;
             }
-            if(next_level != nullptr){
-                next_level->ReadFromAddress(address);
-            }          
+
             if(invalid_block_found){
-                invalid_block->blocks[c0].is_valid=true;
-                invalid_block->blocks[c0].is_dirty=false;
-                invalid_block->blocks[c0].tag_selector=c2;
-                invalid_block->tags[c2]=c3;
-                invalid_block->timestamp=call_count++;
-                return invalid_block->blocks[c0];
+                evictSector(invalid_block,c0,c1,c2,c3,mode);
+
+            }else if(invalid_tag_block_found){
+                evictSector(invalid_tag_block,c0,c1,c2,c3,mode);
                 
             }else{
-                // blocks associated with the old address tag need to be invalidated
-                int addr_tag_to_be_evicted = lru_block->blocks[c0].tag_selector;
-                int i=0;
-                for(vector<Block>::iterator bl_it=lru_block->blocks.begin();bl_it!=lru_block->blocks.end();bl_it++){
-                    if(bl_it->tag_selector==addr_tag_to_be_evicted){                        
-                        if(bl_it->is_valid && bl_it->is_dirty){
-                            num_write_backs++;
-                            num_memory_access++;
-                            if(next_level != nullptr){
-                                int old_addr = (((lru_block->tags[addr_tag_to_be_evicted]*addr_tags+addr_tag_to_be_evicted)*number_of_sets+c1)*data_blocks+i)*block_size;
-                                next_level->WriteToAddress(old_addr);
-                            }
-                        }
-                        bl_it->is_valid=false;
-                    }
-                    i++;
-                }
-                lru_block->blocks[c0].is_valid=true;
-                lru_block->blocks[c0].is_dirty=false;
-                lru_block->blocks[c0].tag_selector=c2;
-                lru_block->tags[c2]=c3;
-                lru_block->timestamp=call_count++; 
-                return lru_block->blocks[c0];
+                evictSector(lru_block,c0,c1,c2,c3,mode);
+            }
+            if(next_level != nullptr){
+                next_level->ReadFromAddress(address);
             }            
         }
-
+        if(cache_level==1){
+            printf("L1 update LRU\n");
+        }else
+        {
+            printf("L2 update LRU\n");
+        }        
     }
 
+
     bool ReadFromAddress(unsigned int address){         
-        FetchBlock(address,'r');
-        // printf("------------------------------------------------\n");
-        // printf("# %d: read %0x\n",line,address);
-        // printf("L1 read: %0x (tag %0x, index %d)\n", address, tag, set_index);
-        // if(is_found){
-        //     printf("L1 hit \n");
-        // }else{
-        //     printf("L1 miss \n");
-        // }        
-        // printf("L1 update LRU\n");
+        FetchBlock(address,'r');        
         return true;
                 
     }
-    bool WriteToAddress(unsigned int address){
-        
-        Block fetchedBlock = FetchBlock(address,'w');
-        fetchedBlock.is_dirty=true;
-        // printf("------------------------------------------------\n");
-        // printf("# %d: write %0x\n",line,address);
-        // printf("L1 write: %0x (tag %0x, index %d)\n", address, tag, set_index);
-        // if(is_found){
-        //     printf("L1 hit \n");
-        // }else{
-        //     printf("L1 miss \n");
-        // }   
-        // printf("L1 set dirty\n");
-        // printf("L1 update LRU\n");
+    bool WriteToAddress(unsigned int address){        
+        FetchBlock(address,'w');              
         return true;
     }
     
@@ -219,7 +284,7 @@ class Cache {
                     }else{
                         valid_char='I';
                     }
-                    printf("\t%d,%c,%c\t",bl_it->tag_selector,valid_char,dirty_char);
+                    printf("\t%0x,%c,%c\t",bl_it->tag_selector,valid_char,dirty_char);
                 } 
                 printf("||");          
             }
@@ -235,8 +300,8 @@ class Cache {
             cout<<"set\t"<<i<<":";
             sort(it->sectors.begin(),it->sectors.end(),CompareTimestamps);
             for(vector<Sector>::iterator sec_it=it->sectors.begin(); sec_it!=it->sectors.end(); sec_it++){ 
-                for(vector<int>::iterator addr_it= sec_it->tags.begin(); addr_it!=sec_it->tags.end();addr_it++){
-                    printf("\t%d\t",*addr_it);
+                for(vector<Tag>::iterator addr_it= sec_it->tags.begin(); addr_it!=sec_it->tags.end();addr_it++){
+                    printf("\t%0x\t",addr_it->tag_id);
                 } 
                 printf("||");               
             }
@@ -259,11 +324,8 @@ class Cache {
                     }else{
                         dirty_char='N';
                     }
-                    printf("\t%0x %c\t||",sec_it->tags[bl_it->tag_selector]*addr_tags+bl_it->tag_selector,dirty_char);
+                    printf("\t%0x %c\t||",sec_it->tags[bl_it->tag_selector].tag_id*addr_tags+bl_it->tag_selector,dirty_char);
                 }
-                
-                
-
             }
             cout<<"\n";
             i++;
@@ -272,6 +334,36 @@ class Cache {
     }
 
 };
+
+void DisplayStats(Cache l1, Cache l2){ 
+    if(l2.addr_tags!=1 || l2.data_blocks!=1){
+        cout<<"\n";
+    }       
+    cout<<"===== Simulation Results =====\n";
+    cout<<"a. number of L1 reads:			"<<l1.num_reads<<"\n";
+    cout<<"b. number of L1 read misses:		"<<l1.num_read_miss<<"\n";
+    cout<<"c. number of L1 writes:			"<<l1.num_writes<<"\n";
+    cout<<"d. number of L1 write misses:		"<<l1.num_write_miss<<"\n";
+    printf("e. L1 miss rate:			%0.4f\n",((float)l1.num_read_miss+(float)l1.num_write_miss)/((float)l1.num_reads+(float)l1.num_writes));        
+    cout<<"f. number of writebacks from L1 memory:	"<<l1.num_write_backs<<"\n";
+    cout<<"g. number of L2 reads:			"<<l2.num_reads<<"\n";
+    cout<<"h. number of L2 read misses:		"<<l2.num_read_miss<<"\n";
+    cout<<"i. number of L2 writes:			"<<l2.num_writes<<"\n";
+    cout<<"j. number of L2 write misses:		"<<l2.num_write_miss<<"\n";
+    if(l2.data_blocks==1 && l2.addr_tags==1){
+        printf("k. L2 miss rate:			%0.4f\n",(float)l2.num_read_miss/(float)l2.num_reads);        
+        cout<<"l. number of writebacks from L2 memory:	"<<l2.num_write_backs<<"\n";
+        cout<<"m. total memory traffic:		"<<l2.num_memory_access<<"\n";
+    }else{
+        cout<<"k. number of L2 sector misses:		"<<l2.sector_miss<<"\n";
+        cout<<"l. number of L2 cache block misses:		"<< l2.num_read_miss + l2.num_write_miss - l2.sector_miss <<"\n";
+        printf("m. L2 miss rate:			%0.4f\n",(float)l2.num_read_miss/(float)l2.num_reads);        
+        cout<<"n. number of writebacks from L2 memory:	"<<l2.num_write_backs<<"\n";
+        cout<<"o. total memory traffic:		"<<l2.num_memory_access<<"\n";
+    }
+    
+    
+}
 
 int main(int argc, char* _argv[])
 {
@@ -295,8 +387,8 @@ int main(int argc, char* _argv[])
         return SYSERR;
     }
     string str;
-    Cache L1_cache = Cache(block_size,l1_size,l1_assoc,1,1);
-    Cache L2_cache = Cache(block_size,l2_size,l2_assoc,l2_data_blocks,l2_addr_tags);
+    Cache L1_cache = Cache(block_size,l1_size,l1_assoc,1,1,1);
+    Cache L2_cache = Cache(block_size,l2_size,l2_assoc,l2_data_blocks,l2_addr_tags,2);
     L1_cache.AttachNextLevelCache(&L2_cache);
     printf("  ===== Simulator configuration =====\n");
     printf("  BLOCKSIZE:                        %d\n",block_size);
@@ -328,13 +420,26 @@ int main(int argc, char* _argv[])
         }        
     }
     infile.close();
-    cout<< "----------------------L1--------------------------\n ";
+
+    cout<< "\n===== L1 contents =====\n ";
     L1_cache.PrintCacheContent();
     cout<<"\n";
-    L1_cache.DisplayStats();    
-    cout<< "----------------------L2--------------------------\n ";
-    L2_cache.PrintCacheContent();
-    cout<<"\n";
-    L2_cache.DisplayStats();   
+    
+    if(l2_data_blocks==1 && l2_addr_tags==1){
+        cout<<"===== L2 contents =====\n";
+        cout<<"";
+        L2_cache.PrintCacheContent();
+        cout<<"\n";
+         
+    }else{        
+        cout<<"===== L2 Address Array contents =====\n";
+        L2_cache.PrintAddressCacheContent();
+        cout<<"\n===== L2 Data Array contents =====\n";
+        L2_cache.PrintDataCacheContent();
+
+    }
+    DisplayStats(L1_cache,L2_cache);  
+    
+    
     return 0;
 }
